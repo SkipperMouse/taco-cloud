7b51c5a6076cdf0638f44765c97614f5c0f8944d
Sokolov Roman <sokolov1992r@gmail.com>
2023-01-20 20:58:31 +0400

    Implementation of Spring Data JPA
    Now all comunications with database go through repository interfaces
    There was a bug that not run db scripts to h2. It was resolved by adding new bean
    to TacoCloudApplication.java it returns ApplicationRunner class that insert
     Ingredients into db by using JPA Repository

--- src/main/java/sia/tacocloud/model/Ingredient.java
+++ src/main/java/sia/tacocloud/model/Ingredient.java
@@ -1,15 +1,48 @@
 package sia.tacocloud.model;
 
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@Entity(name = "Ingredient")
 public class Ingredient {
-    private final String id;
-    private final String name;
-    private final Type type;
+    @Id
+    @Column(name = "id", nullable = false)
+    private String id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "type")
+    @Enumerated(value = EnumType.STRING)
+    private Type type;
 
+    public Ingredient() {
+    }
+
+    public Ingredient(String id, String name, Type type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Ingredient that = (Ingredient) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
--- src/main/java/sia/tacocloud/model/Taco.java
+++ src/main/java/sia/tacocloud/model/Taco.java
@@ -1,23 +1,56 @@
 package sia.tacocloud.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@Entity(name = "Taco")
 public class Taco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
 
+
+    @Column(name = "name")
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
+    @ManyToMany
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 
+
+    @Column(name = "created_at")
     private Date createdAt = new Date();
+
+    public void addIngredient(Ingredient ingredient){
+        ingredients.add(ingredient);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Taco taco = (Taco) o;
+        return id != null && Objects.equals(id, taco.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
