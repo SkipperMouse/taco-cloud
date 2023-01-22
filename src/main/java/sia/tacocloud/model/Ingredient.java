04626c66ebf3b7a37143b0453fcea95fdcc7a56d
Sokolov Roman <sokolov1992r@gmail.com>
2023-01-22 18:45:11 +0400

    Remove JPA annotation from model

--- src/main/java/sia/tacocloud/model/Ingredient.java
+++ src/main/java/sia/tacocloud/model/Ingredient.java
@@ -1,23 +1,15 @@
 package sia.tacocloud.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.Hibernate;
-
-import java.util.Objects;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
-@Entity(name = "Ingredient")
 public class Ingredient {
-    @Id
-    @Column(name = "id", nullable = false)
     private String id;
-    @Column(name = "name")
     private String name;
-    @Column(name = "type")
-    @Enumerated(value = EnumType.STRING)
     private Type type;
 
     public Ingredient() {
@@ -33,16 +25,4 @@ public class Ingredient {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Ingredient that = (Ingredient) o;
-        return id != null && Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
--- src/main/java/sia/tacocloud/model/TacoOrder.java
+++ src/main/java/sia/tacocloud/model/TacoOrder.java
@@ -1,6 +1,5 @@
 package sia.tacocloud.model;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -14,57 +13,44 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@Entity(name = "Taco_Order")
 @Getter
 @Setter
 public class TacoOrder implements Serializable {
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "delivery_name")
     @NotBlank(message = "Delivery name is required")
     @Size(max = 50, message = "Maximum size of the field is 50 characters")
     private String deliveryName;
 
-    @Column(name = "delivery_street")
     @NotBlank(message = "Street is required")
     @Size(max = 50, message = "Maximum size of the field is 50 characters")
     private String deliveryStreet;
 
-    @Column(name = "delivery_city")
     @NotBlank(message = "City is required")
     @Size(max = 50, message = "Maximum size of the field is 50 characters")
     private String deliveryCity;
 
-    @Column(name = "delivery_state")
     @NotBlank(message = "State is required")
     @Size(max = 2, message = "Maximum size of the field is 2 characters")
     private String deliveryState;
 
-    @Column(name = "delivery_zip")
     @NotBlank(message = "Zip code is required")
     @Size(max = 10, message = "Maximum size of the field is 10 characters")
     private String deliveryZip;
 
-    @Column(name = "cc_number")
     @CreditCardNumber(message = "Not a valid card number")
     private String ccNumber;
 
-    @Column(name = "cc_expiration")
     @Pattern(regexp = "(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
             message = "Must be format MM/YY")
     private String ccExpiration;
 
-    @Column(name = "cc_cvv")
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
-    @OneToMany(cascade = CascadeType.ALL)
     private List<Taco> tacos;
 
-    @Column(name = "placed_at")
     private Date placedAt;
 
     public void addTaco(Taco taco) {
