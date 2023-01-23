11a4ce367a861f942cb84305254c162278ae6893
Sokolov Roman <sokolov1992r@gmail.com>
2023-01-23 13:01:12 +0400

    Refactor security config to work with User entity

--- src/main/java/sia/tacocloud/config/SecurityConfig.java
+++ src/main/java/sia/tacocloud/config/SecurityConfig.java
@@ -2,32 +2,28 @@ package sia.tacocloud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import sia.tacocloud.model.User;
+import sia.tacocloud.repository.UserRepository;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder){
-        List<UserDetails> userList = new ArrayList<>();
-        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
-        userList.add(new User("skipper" , encoder.encode("password"), authorities));
-        userList.add(new User("roman", encoder.encode("password"), authorities));
-        return new InMemoryUserDetailsManager(userList);
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            User user = userRepository.findByUsername(username);
+            if (user != null) {
+                return user;
+            } else throw new UsernameNotFoundException(String.format("User %s not found", username));
+        };
     }
 }
