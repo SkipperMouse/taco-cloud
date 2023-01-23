97cac9a0453793b48ffd615346cf8a6fb6785c70
Sokolov Roman <sokolov1992r@gmail.com>
2023-01-23 11:41:30 +0400

    Add security config and inmemory realization
    Now program has two users - Roman and skipper. They are placed in memory and
    if you need to add new User you will need to recompile the app.
    It is good for testing. But it is unacceptable for production. So the next step
    is to rewrite this code for multiplies users

---
+++ src/main/java/sia/tacocloud/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package sia.tacocloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        List<UserDetails> userList = new ArrayList<>();
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        userList.add(new User("skipper" , encoder.encode("password"), authorities));
+        userList.add(new User("roman", encoder.encode("password"), authorities));
+        return new InMemoryUserDetailsManager(userList);
+    }
+}
